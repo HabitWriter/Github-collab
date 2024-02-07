68e2922ce9511e17599d160e678ef153c6738e82
Robert Smith <r.reed.smith@gmail.com>
2024-02-06 21:03:00 -0700

    pushing new repo

---
+++ src/PageTwo.jsx
@@ -0,0 +1,21 @@
+export default function PageTwo() {
+
+
+  return (
+    <>
+    <div className="body">
+    <div className="body-card">
+      <div className="result-card">
+        <h2>Your Result</h2>
+        </div>
+      <div className="summary-card">
+        <h2>Summary</h2>
+      </div>
+
+
+      
+    </div>
+    </div>
+    </>
+  );
+}
