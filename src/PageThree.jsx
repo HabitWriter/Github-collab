8e4a2c3ce1b4f6b945581d78871fa34db51f6b85
wheelejay <wheeler7591@gmail.com>
2024-02-06 21:06:39 -0700

    Added source folder and image to pageThree

---
+++ src/PageThree.jsx
@@ -0,0 +1,12 @@
+
+export default function PageThree() {
+
+
+  return (
+    <>
+      <div>
+        <img src={'./social-links-profile-main/assets/images/avatar-jessica.jpeg'}>Jessica Randall  </img>
+      </div>
+    </>
+  );
+}
